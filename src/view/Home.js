f6b640d73f53048754797e5bea130e49f60da28b
aicoder <malun666@126.com>
2019-03-03 19:26:44 +0800

    added the whole home file

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { Prompt } from 'react-router-dom';
-
+import { Link, Route, Switch } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+import About from './About';
+import Product from './Product';
 class Home extends Component {
   logout = () => {
     // 清楚sessionStorage里面的用户登录的信息。
@@ -9,15 +11,60 @@ class Home extends Component {
   }
 
   render () {
-    // console.log(this.props);
+    let { match } = this.props;
     return (
       <div>
-        Home
-        <hr/>
-        <button
-          onClick={ this.logout }
-          className="button is-danger"
-        >登出</button>
+        {/* 网站顶部导航 */}
+        <nav className="navbar">
+          <div className="navbar-band">
+            <Link to="/app"><img src={Logo} alt="aicoder.com官网" width="130" title="有利于网站的SEO" /></Link>
+          </div>
+          <div className="navbar-menu">
+            <div className="navbar-start">
+              <Link className="navbar-item" to="/app">首页</Link>
+              <Link className="navbar-item" to={ `${match.path}/product` }>产品</Link>
+              <Link className="navbar-item" to={ `${match.path}/cases` }>成功案例</Link>
+              <Link className="navbar-item" to={ `${match.path}/about` }>关于</Link>
+            </div>
+            <div className="navbar-end">
+              <button
+                onClick={ this.logout }
+                className="button is-danger"
+              >登出
+              </button>
+            </div>
+          </div>
+        </nav>
+
+        {/* 网站主要内容区域 */}
+        <main className="columns">
+          <div className="menu-list column is-one-fifth has-background-info">
+              <Link className="navbar-item" to="/app">首页</Link>
+              <br/>
+              <Link className="navbar-item" to={ `${match.path}/product` }>产品</Link>
+              <br/>
+              <Link className="navbar-item" to={ `${match.path}/cases` }>成功案例</Link>
+              <br/>
+              <Link className="navbar-item" to={ `${match.path}/about` }>关于</Link>
+          </div>
+          <div className="column has-background-primary">
+            <Switch>
+              <Route path={`${match.path}/about`} component={About}></Route>
+              <Route path={`${match.path}/product`} component={Product}></Route>
+              <Route render={ () => {
+                return (
+                  <div className="hero">
+                    <h3 className="title">欢迎您访问aicoder.com</h3>
+                    <p className="subtitle">您的访问将是我们的荣耀！</p>
+                  </div>
+                )
+              }}></Route>
+            </Switch>
+          </div>
+        </main>
+        <div className="footer has-backgroud-light">
+          版权所有@aicoder.com
+        </div>
       </div>
     )
   }
