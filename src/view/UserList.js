8fb487964f65d71263402da69b3cefd179e3b49f
aicoder <malun666@126.com>
2019-03-06 21:04:54 +0800

    added the userlist

--- src/view/UserList.js
+++ src/view/UserList.js
@@ -4,6 +4,20 @@ import store from '../store';
 import { UserListActionCreators } from '../actions/UserListAction';
 
 class UserList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      UserList: store.getState().UserList,
+      unsubscribe: store.subscribe(() => {
+        this.setState({
+          UserList: store.getState().UserList
+        })
+      })
+    };
+  }
+  componentWillUnmount() {
+    this.state.unsubscribe(); // 移除监听 
+  }
 
   componentDidMount() {
     // 加载数据
@@ -19,7 +33,31 @@ class UserList extends Component {
   render () {
     return (
       <div>
-        uselist
+      <h3 className="title">用户列表</h3>
+      <table className="table is-striped is-hoverable is-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>用户名</th>
+            <th>地址</th>
+            <th>电话</th>
+            <th>是否删除</th>
+            <th>备注</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.state.UserList.map( (item, index) => 
+            <tr key={index}>
+              <td>{ item.Id }</td>
+              <td>{ item.UserName }</td>
+              <td>{ item.Address }</td>
+              <td>{ item.Phone }</td>
+              <td>{ item.Del ? '是' : '否' }</td>
+              <td>{ item.Remark }</td>
+            </tr>
+           )}
+        </tbody>
+      </table>
       </div>
     )
   }
